bf5d6d20cc99925f15920a1b74984941d8545edc
Tiffany Ma <tma@fb.com>
2018-06-27 12:01:03 -0700

    Network initial setup

--- Flix/ViewController/MovieViewController.h
+++ Flix/ViewController/MovieViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MovieViewController.h
 //  Flix
 //
 //  Created by Tiffany Ma on 6/27/18.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MovieViewController : UIViewController
 
 @end
-
---
+++ Flix/ViewController/MovieViewController.m
@@ -0,0 +1,56 @@
+//
+//  MovieViewController.m
+//  Flix
+//
+//  Created by Tiffany Ma on 6/27/18.
+//  Copyright © 2018 Tiffany Ma. All rights reserved.
+//
+
+#import "MovieViewController.h"
+
+@interface MovieViewController ()
+
+@end
+
+@implementation MovieViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error != nil) {
+            NSLog(@"%@", [error localizedDescription]);
+        }
+        else {
+            NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+            
+            NSLog(@"@%@", dataDictionary);
+            // TODO: Get the array of movies
+            // TODO: Store the movies in a property to use elsewhere
+            // TODO: Reload your table view data
+        }
+    }];
+    [task resume];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
