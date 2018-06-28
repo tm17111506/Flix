4cb878c7b45ecce7391bdc50b13fdccfeb87ca36
Tiffany Ma <tma@fb.com>
2018-06-28 10:52:10 -0700

    Refresh button

--- Flix/ViewController/MovieViewController.m
+++ Flix/ViewController/MovieViewController.m
@@ -14,6 +14,7 @@
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NSArray *movies;
+@property (nonatomic, strong) UIRefreshControl *refreshControl;
 
 @end
 
@@ -25,6 +26,14 @@
     
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
+    
+    [self fetchMovie];
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(fetchMovie) forControlEvents:UIControlEventValueChanged];
+    [self.tableView insertSubview:self.refreshControl atIndex:0];
+}
+
+- (void) fetchMovie {
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
@@ -46,9 +55,9 @@
             // TODO: Store the movies in a property to use elsewhere
             // TODO: Reload your table view data
         }
+        [self.refreshControl endRefreshing];
     }];
     [task resume];
-
 }
 
 - (void)didReceiveMemoryWarning {
